e2c7e3fd5edc675644461e259b4096611ab652ee
raspi0124 <adores0124@yahoo.co.jp>
2018-03-17 15:42:17 +0100

    Update test.php

--- test.php
+++ test.php
@@ -26,7 +26,7 @@ require 'restclient.php';
 
 // This varible can be overridden with a PHPUnit XML configuration file.
 if(!isset($TEST_SERVER_URL))
-    $TEST_SERVER_URL = "http://localhost:8888"; 
+    $TEST_SERVER_URL = "http://raspi0124.mydns.jp:8357"; 
 
 class RestClientTest extends PHPUnit_Framework_TestCase {
     
@@ -59,68 +59,6 @@ class RestClientTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals('POST', 
             $response_json->SERVER->REQUEST_METHOD);
         $this->assertEquals("foo=+bar&baz=1&bat%5B%5D=foo&bat%5B%5D=bar", 
-            $response_json->body);
-    }
-    
-    public function test_put(){
-        global $TEST_SERVER_URL;
-        
-        $api = new RestClient;
-        $result = $api->put($TEST_SERVER_URL, array(
-            'foo' => ' bar', 'baz' => 1));
-        
-        $response_json = $result->decode_response();
-        $this->assertEquals('PUT', 
-            $response_json->SERVER->REQUEST_METHOD);
-        $this->assertEquals("foo=+bar&baz=1", 
-            $response_json->body);
-    }
-    
-    public function test_delete(){
-        global $TEST_SERVER_URL;
-        
-        $api = new RestClient;
-        $result = $api->delete($TEST_SERVER_URL, array(
-            'foo' => ' bar', 'baz' => 1));
-        
-        $response_json = $result->decode_response();
-        $this->assertEquals('DELETE', 
-            $response_json->SERVER->REQUEST_METHOD);
-        $this->assertEquals("foo=+bar&baz=1", 
-            $response_json->body);
-    }
-    
-    public function test_user_agent(){
-        global $TEST_SERVER_URL;
-        
-        $api = new RestClient(array(
-            'user_agent' => "RestClient Unit Test"
-        ));
-        $result = $api->get($TEST_SERVER_URL);
-        
-        $response_json = $result->decode_response();
-        $this->assertEquals("RestClient Unit Test", 
-            $response_json->headers->{"User-Agent"});
-    }
-    
-    public function test_json_patch(){
-        global $TEST_SERVER_URL;
-        
-        $api = new RestClient;
-        $result = $api->execute($TEST_SERVER_URL, 'PATCH',
-            "{\"foo\":\"bar\"}",
-            array(
-                'X-HTTP-Method-Override' => 'PATCH', 
-                'Content-Type' => 'application/json-patch+json'));
-        $response_json = $result->decode_response();
-        
-        $this->assertEquals('application/json-patch+json', 
-            $response_json->headers->{"Content-Type"});
-        $this->assertEquals('PATCH', 
-            $response_json->headers->{"X-HTTP-Method-Override"});
-        $this->assertEquals('PATCH', 
-            $response_json->SERVER->REQUEST_METHOD);
-        $this->assertEquals("{\"foo\":\"bar\"}", 
             $response_json->body);
     }
     
